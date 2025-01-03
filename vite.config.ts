99d4b21c11f56185e7e20f959068daa7a8ba9708
Zaw Lin Naing <zawlinnnaing0018@gmail.com>
2025-01-03 11:46:22 +0700

    feat: added offline support (#8)
    * feat: added offline support with service worker
    
    * docs: update readme
    
    * No code changes detected

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "tailwindcss";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: ["**/*"],
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   base: "/",
   resolve: {
     alias: {
